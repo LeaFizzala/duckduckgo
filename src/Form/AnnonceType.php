e7b1c4cd262d2aa4df6c0b784184ff9433c345c3
LeaFizzala <leafizzala.dev@gmail.com>
2022-11-28 13:55:55 +0100

    Added edition and creation form

---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('price' )
+            ->add('status', ChoiceType::class,
+            [
+                'choices' => [
+                    'Very bad' => Annonce::STATUS_VERY_BAD,
+                    'Bad' => Annonce::STATUS_BAD,
+                    'Good' =>  Annonce::STATUS_GOOD,
+                    'Very Good' =>  Annonce::STATUS_VERY_GOOD,
+                    'Perfect' =>  Annonce::STATUS_PERFECT,
+                ]
+            ])
+            ->add('createdAt', DateType::class)
+            ->add('sold')
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+            'translation_domain' => 'forms'
+            // ici on peut passer toutes les options qu'on veut
+
+        ]);
+    }
+}
